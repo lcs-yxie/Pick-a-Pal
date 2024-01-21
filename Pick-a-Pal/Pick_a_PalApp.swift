b747d41f8c3d05a729085d656b70ac7f0215a41a
Yukun Xie <yxie@lcs.on.ca>
2024-01-21 16:32:10 -0500

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Yukun Xie on 2024/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

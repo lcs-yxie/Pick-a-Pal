76f2d93f24529a9d2d0749db77f81f8a77309689
Yukun Xie <yxie@lcs.on.ca>
2024-01-21 16:46:17 -0500

    Created a list for the names

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -7,18 +7,73 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var names: [String] = ["Kevin", "John", "Lee"]
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
+            }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Toggle("Remove picked names", isOn: $shouldRemovePickedName)
+            
+            Button {
+                if names.count > 0 {
+                    pickedName = names.randomElement()!
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == pickedName)
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+            } label: {
+                Text("Pick Random Name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
+/
